b0d88e690dbf310968bdc3367450d579c3719f8f
NamLa <nam.darkstar@gmail.com>
2021-06-06 02:59:23 +0700

    update

---
+++ WebBanDienThoai/WebBanDienThoai/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private WebDBEntities1 db = new WebDBEntities1();
+        public ActionResult LoginAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginAccount(string email, string password)
+        {
+            //Md5Encode md5 = new Md5Encode();
+            //var passmd5 = md5.EncodeMd5Encrypt(password);
+            var login = db.Users.SingleOrDefault(x => x.Email == email && x.Password == password && x.Status == true);
+
+            if (login != null)
+            {
+
+
+                Session["UserID"] = login.UserID;
+                Session["Username"] = login.Username;
+                Session["Email"] = login.Email;
+                Session["Password"] = login.Password;
+                Session["Image"] = login.Image;
+                return Redirect("~/AdminHome/Index");
+
+
+            }
+            else
+            {
+
+                ViewBag.error = "Tên Tài Khoản Hoặc Mật Khẩu Không Đúng";
+            }
+            return View();
+        }
+    }
+}
---
+++ WebBanDienThoai/WebBanDienThoai/Models/ProductCart.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Models
+{
+    public class ProductCart
+    {
+        WebDBEntities1 db = null;
+
+        public ProductCart()
+        {
+            db = new WebDBEntities1();
+        }
+        public Product ViewDetail(int ProductID)
+        {
+            return db.Products.Find(ProductID);
+        }
+    }
+}
